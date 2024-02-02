85d4123679e6d9b59b7b5272f7640eb86f4b4ba8
Jason K. Moore <moorepants@gmail.com>
2024-02-02 06:39:10 +0100

    Use different var names for tables.

--- src/schmidt_bike_controller.py
+++ src/schmidt_bike_controller.py
@@ -48,14 +48,14 @@ Gunity = cn.TransferFunction(1, 1, s)
 Ginner = cn.Feedback(Dcont*Gdelta, Gtheta)
 char_eq = Ginner.doit().simplify().expand().den
 
-tab = routh_table(char_eq, s)
+tab_inner = routh_table(char_eq, s)
 
-sm.pprint(tab[:, 0])
+sm.pprint(tab_inner[:, 0])
 
 # NOTE : Shouldn't have to call .doit() on Ginner for this to work.
 Gouter = cn.Feedback(PIcont*Ginner.doit()*Gpsi, Gunity)
 char_eq = Gouter.doit().simplify().expand().den
 
-tab = routh_table(char_eq, s)
+tab_outer = routh_table(char_eq, s)
 
-sm.pprint(tab[:, 0])
+sm.pprint(tab_outer[:, 0])
